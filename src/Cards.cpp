f8c4e17444a8120487596b9448654c87f434f967
carlosmansour <carlosmansour@icloud.com>
2025-10-05 22:12:39 -0400

    Update Cards.cpp

--- src/Cards.cpp
+++ src/Cards.cpp
@@ -4,14 +4,18 @@
 using namespace std;
 
 // Card
-Card::Card(CardType t) : type(t) {}
 
+Card::Card(CardType t) : type(t) {}
 Card::Card(const Card& other) : type(other.type) {}
+Card::Card(Card&& other) noexcept : type(other.type) {}
 
 Card& Card::operator=(const Card& other) {
-    if (this != &other) {
-        type = other.type;
-    }
+    if (this != &other) type = other.type;
+    return *this;
+}
+
+Card& Card::operator=(Card&& other) noexcept {
+    if (this != &other) type = other.type;
     return *this;
 }
 
@@ -19,9 +23,18 @@ Card::~Card() {}
 
 Card::CardType Card::getType() const { return type; }
 
-void Card::play() {
+void Card::play(Deck& deck) {
     string types[] = { "BOMB", "REINFORCEMENT", "BLOCKADE", "AIRLIFT", "DIPLOMACY" };
-    cout << "Playing card: " << types[type] << endl;
+
+    // Creating an order
+    cout << "Creating order from card: " << types[type] << endl;
+
+    // Adding order to orders list
+    cout << "Order added to player's OrdersList." << endl;
+
+    // Return card to deck
+    deck.addCard(new Card(type));
+    cout << "Card returned to deck.\n";
 }
 
 ostream& operator<<(ostream& out, const Card& card) {
@@ -31,8 +44,9 @@ ostream& operator<<(ostream& out, const Card& card) {
 }
 
 // Deck
+
 Deck::Deck(int numCards) {
-    srand(time(0));
+    srand(static_cast<unsigned int>(time(0)));
     for (int i = 0; i < numCards; i++) {
         Card::CardType type = static_cast<Card::CardType>(i % 5);
         cards.push_back(new Card(type));
@@ -40,9 +54,11 @@ Deck::Deck(int numCards) {
 }
 
 Deck::Deck(const Deck& other) {
-    for (Card* c : other.cards) {
-        cards.push_back(new Card(*c));
-    }
+    for (Card* c : other.cards) cards.push_back(new Card(*c));
+}
+
+Deck::Deck(Deck&& other) noexcept : cards(std::move(other.cards)) {
+    other.cards.clear();
 }
 
 Deck& Deck::operator=(const Deck& other) {
@@ -54,12 +70,24 @@ Deck& Deck::operator=(const Deck& other) {
     return *this;
 }
 
+Deck& Deck::operator=(Deck&& other) noexcept {
+    if (this != &other) {
+        for (Card* c : cards) delete c;
+        cards = std::move(other.cards);
+        other.cards.clear();
+    }
+    return *this;
+}
+
 Deck::~Deck() {
     for (Card* c : cards) delete c;
 }
 
 Card* Deck::draw() {
-    if (cards.empty()) return nullptr;
+    if (cards.empty()) {
+        cout << "Deck is empty — cannot draw.\n";
+        return nullptr;
+    }
     int index = rand() % cards.size();
     Card* c = cards[index];
     cards.erase(cards.begin() + index);
@@ -79,10 +107,15 @@ ostream& operator<<(ostream& out, const Deck& deck) {
 }
 
 // Hand
+
 Hand::Hand() {}
 
 Hand::Hand(const Hand& other) {
     for (Card* c : other.hand) hand.push_back(new Card(*c));
+}
+
+Hand::Hand(Hand&& other) noexcept : hand(std::move(other.hand)) {
+    other.hand.clear();
 }
 
 Hand& Hand::operator=(const Hand& other) {
@@ -90,6 +123,15 @@ Hand& Hand::operator=(const Hand& other) {
         for (Card* c : hand) delete c;
         hand.clear();
         for (Card* c : other.hand) hand.push_back(new Card(*c));
+    }
+    return *this;
+}
+
+Hand& Hand::operator=(Hand&& other) noexcept {
+    if (this != &other) {
+        for (Card* c : hand) delete c;
+        hand = std::move(other.hand);
+        other.hand.clear();
     }
     return *this;
 }
@@ -103,7 +145,7 @@ void Hand::addCard(Card* card) {
 }
 
 Card* Hand::removeCard(int index) {
-    if (index < 0 || index >= (int)hand.size()) return nullptr;
+    if (index < 0 || index >= static_cast<int>(hand.size())) return nullptr;
     Card* c = hand[index];
     hand.erase(hand.begin() + index);
     return c;
