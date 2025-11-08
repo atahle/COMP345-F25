5f42543422b8b9dd90882d8f0069892a49e61381
abdellahcs <155846993+abdellahcs@users.noreply.github.com>
2025-11-08 15:23:37 -0500

    Update MainDriver.cpp

--- src/MainDriver.cpp
+++ src/MainDriver.cpp
@@ -11,18 +11,20 @@ int main() {
     int choice = -1;
 
     do {
+        std::cout << "-----------------------------------------\n";
         std::cout << "  [1] - Run testLoadMaps\n";
         std::cout << "  [2] - Run testPlayers\n";
         std::cout << "  [3] - Run testOrdersLists\n";
         std::cout << "  [4] - Run testCards\n";
         std::cout << "  [5] - Run testGameStates\n";
-        std::cout << "  [6] - Run ALL tests (sequential)\n";
+        std::cout << "  [6] - Run testStartupPhase\n";
+        std::cout << "  [7] - Run ALL tests (sequential)\n";
         std::cout << "  [0] - Quit Program\n";
-        std::cout << "---------------------------------------\n";
-        std::cout << "Enter your choice (0-5): ";
+        std::cout << "-----------------------------------------\n";
+        std::cout << "Enter your choice (0-7): ";
 
         if (!(std::cin >> choice)) {
-            std::cout << "\n[Error] Invalid input. Please enter a number (0-5).\n";
+            std::cout << "\n[Error] Invalid input. Please enter a number (0-7).\n";
             std::cin.clear();
             std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
             choice = -1; 
@@ -45,13 +47,16 @@ int main() {
         case 5:
             testGameStates();
             break;
-        case 6:
+        case 6: 
+            testStartupPhase(); break;
+        case 7:
             std::cout << "\n[Running all tests...]\n";
             testLoadMaps();
             testPlayers();
             testOrdersLists();
             testCards();
             testGameStates();
+            testStartupPhase();
             break;
         case 0:
             std::cout << "\n[Program Exit] Thank you for using the menu!\n";
